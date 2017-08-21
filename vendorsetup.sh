73a6e33b942128ffc743e83a825e85734688e9fd
JagravNaik <jagravnaik0@gmail.com>
2017-08-21 11:03:19 -0400

    add Mi 5(gemini) to the bunch!
    add user lunch combo for new device

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,8 @@
-add_lunch_combo jos_oneplus3-userdebug
-add_lunch_combo jos_oneplus3-user
+add_lunch_combo jos_gemini-userdebug
+add_lunch_combo jos_gemini-user
 add_lunch_combo jos_herolte-userdebug
 add_lunch_combo jos_herolte_user
 add_lunch_combo jos_hero2lte-userdebug
 add_lunch_combo jos_hero2lte-user
+add_lunch_combo jos_oneplus3-userdebug
+add_lunch_combo jos_oneplus3-user
