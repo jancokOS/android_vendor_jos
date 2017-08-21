c83b4ff3961892544810b81a27a4a95975146acd
JagravNaik <jagravnaik0@gmail.com>
2017-08-21 10:58:21 -0400

    add user lunch combo for all supported devices
    user builds are more optimized for battery and performance!

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,6 @@
 add_lunch_combo jos_oneplus3-userdebug
+add_lunch_combo jos_oneplus3-user
 add_lunch_combo jos_herolte-userdebug
+add_lunch_combo jos_herolte_user
 add_lunch_combo jos_hero2lte-userdebug
+add_lunch_combo jos_hero2lte-user
